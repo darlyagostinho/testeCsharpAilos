fc8d46854a8c97e3f98053587580e94bd7978fb5
Darly Agostinho <darly.agostinho@amcom.com.br>
2024-08-26 09:17:48 -0300

    Questões 1 a 4 resolvidas

---
+++ Questao1/ContaBancaria.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Questao1
+{
+    class ContaBancaria {
+        private int _numero { get; }
+        public string Titular { get; set; }
+        private double _saldo { get; set; }
+        private float _taxa { get; set; } = 3.5f;
+
+        public ContaBancaria(int numero, string titular, double depositoInicial = 0.0) { 
+            _numero = numero;
+            Titular = titular;
+            _saldo = depositoInicial;
+        }
+
+        public void Deposito(double quantia)
+        {
+            if (quantia > 0.0)
+            {
+               _saldo += quantia;
+            }
+        }
+
+        public void Saque(double quantia)
+        {
+            if (quantia > 0.0)
+            {
+                _saldo -= _taxa;
+                _saldo -= quantia;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Conta {_numero}, Titular: {Titular}, Saldo: $ {_saldo.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
---
+++ Questao2/Program.cs
@@ -0,0 +1,83 @@
+﻿using Newtonsoft.Json.Linq;
+
+
+public class Program
+{
+    public static void Main()
+    {
+        string teamName = "Paris Saint-Germain";
+        int year = 2013;
+        int totalGoals = getTotalScoredGoals(teamName, year);
+
+        Console.WriteLine("Team " + teamName + " scored " + totalGoals.ToString() + " goals in " + year);
+
+        teamName = "Chelsea";
+        year = 2014;
+        totalGoals = getTotalScoredGoals(teamName, year);
+
+        Console.WriteLine("Team " + teamName + " scored " + totalGoals.ToString() + " goals in " + year);
+
+        // Output expected:
+        // Team Paris Saint - Germain scored 109 goals in 2013
+        // Team Chelsea scored 92 goals in 2014
+    }
+
+    public static int getTotalScoredGoals(string team, int year)
+    {
+        string baseUrl = $@"https://jsonmock.hackerrank.com/api/football_matches";
+        string[] teams = { "team1", "team2" };
+        HttpClient client = new HttpClient();
+        int totalGoals = 0;
+        int totalPages = 0;
+
+        try
+        {
+            string url = $"{baseUrl}?year={year}&{teams[0]}={team}";
+
+            HttpResponseMessage response = client.GetAsync(url).GetAwaiter().GetResult();
+            response.EnsureSuccessStatusCode();
+
+            string responseBody = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+
+            JObject json = JObject.Parse(responseBody);
+
+            totalPages = json["total_pages"].ToObject<int>();
+        }
+        catch (HttpRequestException e)
+        {
+            Console.WriteLine("Erro na requisição: " + e.Message);
+        }
+
+        for (int i = 1; i <= totalPages; i++)
+        {
+            foreach (var teamParameter in teams)
+            {
+                try
+                {
+                    string url = $"{baseUrl}?year={year}&{teamParameter}={team}&page={i}";
+
+                    HttpResponseMessage response = client.GetAsync(url).GetAwaiter().GetResult();
+                    response.EnsureSuccessStatusCode();
+
+                    string responseBody = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+
+                    JObject json = JObject.Parse(responseBody);
+
+                    foreach (var item in json["data"])
+                    {
+                        int teamGoals = item[$"{teamParameter}goals"].ToObject<int>();
+                        totalGoals += teamGoals;
+                    }
+                }
+                catch (HttpRequestException e)
+                {
+                    Console.WriteLine("Erro na requisição: " + e.Message);
+                }
+
+            }
+        }
+
+        return totalGoals;
+    }
+
+}
